053df7d2b2a2c16b7cb4ff7305cc70375151822e
ocvb <electrokai1@gmail.com>
2022-02-03 13:59:41 +0800

    efef

---
+++ admin_check.php
@@ -0,0 +1,29 @@
+<?php
+include_once "db.php";
+
+$sql = "SELECT ID, email, administrator FROM members";
+$result = mysqli_query($db, $sql);
+
+while (mysqli_num_rows($result) == true) {
+   $row = mysqli_fetch_assoc($result);
+   $
+   if ($_COOKIE['user'] == $row['email'] && $row['administrator'] == 'yes') {
+      setcookie("adStatus", 'yes', null, "/");
+   } else {
+      setcookie("adStatus", 'no', null, "/");
+   }
+}
+
+?>
+
+<!--<head>
+   <script src="js/jquery-3.6.0.min.js" type="application/javascript"></script>
+   <script type="module">
+      import { getCookie } from "./js/cookies.js";
+
+      $(document).ready(function() {
+         if (getCookie("user") == 'admin%40admin.com') {
+         }
+      });
+      </script>
+</head>-->
